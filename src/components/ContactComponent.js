5cd8c852526bb6db273f6b196e942b9112d77f79
Minh Hoang <daominhhoangggg@gmail.com>
2022-08-14 13:55:20 +0700

    Lab06_3

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, FormFeedback, Label, Input, Col } from 'reactstrap';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Button,
+  Form,
+  FormGroup,
+  FormFeedback,
+  Label,
+  Input,
+  Col,
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 class Contact extends Component {
@@ -56,20 +66,31 @@ class Contact extends Component {
       email: '',
     };
 
-    if (this.state.touched.firstname && firstname.length < 3) errors.firstname = 'First Name should be >= 3 characters';
-    else if (this.state.touched.firstname && firstname.length > 10) errors.firstname = 'First Name should be <= 10 characters';
+    if (this.state.touched.firstname && firstname.length < 3)
+      errors.firstname = 'First Name should be >= 3 characters';
+    else if (this.state.touched.firstname && firstname.length > 10)
+      errors.firstname = 'First Name should be <= 10 characters';
 
-    if (this.state.touched.lastname && lastname.length < 3) errors.lastname = 'Last Name should be >= 3 characters';
-    else if (this.state.touched.lastname && lastname.length > 10) errors.lastname = 'Last Name should be <= 10 characters';
+    if (this.state.touched.lastname && lastname.length < 3)
+      errors.lastname = 'Last Name should be >= 3 characters';
+    else if (this.state.touched.lastname && lastname.length > 10)
+      errors.lastname = 'Last Name should be <= 10 characters';
 
     const reg = /^\d+$/;
-    if (this.state.touched.telnum && !reg.test(telnum)) errors.telnum = 'Tel. Number should contain only numbers';
+    if (this.state.touched.telnum && !reg.test(telnum))
+      errors.telnum = 'Tel. Number should contain only numbers';
 
-    if (this.state.touched.email && email.split('').filter(x => x === '@').length !== 1) errors.email = 'Email should contain a @';
+    if (this.state.touched.email && email.split('').filter(x => x === '@').length !== 1)
+      errors.email = 'Email should contain a @';
   }
 
   render() {
-    const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
+    const errors = this.validate(
+      this.state.firstname,
+      this.state.lastname,
+      this.state.telnum,
+      this.state.email
+    );
     return (
       <div className="container">
         <div className="row">
@@ -101,7 +122,8 @@ class Contact extends Component {
               <br />
               <i className="fa fa-fax"></i>: +852 8765 4321
               <br />
-              <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+              <i className="fa fa-envelope"></i>:{' '}
+              <a href="mailto:confusion@food.net">confusion@food.net</a>
             </address>
           </div>
           <div className="col-12 col-sm-6 offset-sm-1">
@@ -207,13 +229,22 @@ class Contact extends Component {
                 <Col md={{ size: 6, offset: 2 }}>
                   <FormGroup check>
                     <Label check>
-                      <Input type="checkbox" name="agree" checked={this.state.agree} onChange={this.handleInputChange} />{' '}
+                      <Input
+                        type="checkbox"
+                        name="agree"
+                        checked={this.state.agree}
+                        onChange={this.handleInputChange}
+                      />{' '}
                       <strong>May we contact you?</strong>
                     </Label>
                   </FormGroup>
                 </Col>
                 <Col md={{ size: 3, offset: 1 }}>
-                  <Input type="select" name="contactType" value={this.state.contactType} onChange={this.handleInputChange}>
+                  <Input
+                    type="select"
+                    name="contactType"
+                    value={this.state.contactType}
+                    onChange={this.handleInputChange}>
                     <option>Tel.</option>
                     <option>Email</option>
                   </Input>
@@ -224,7 +255,13 @@ class Contact extends Component {
                   Your Feedback
                 </Label>
                 <Col md={10}>
-                  <Input type="textarea" id="message" name="message" rows="12" value={this.state.message} />
+                  <Input
+                    type="textarea"
+                    id="message"
+                    name="message"
+                    rows="12"
+                    value={this.state.message}
+                  />
                 </Col>
               </FormGroup>
               <FormGroup row>
