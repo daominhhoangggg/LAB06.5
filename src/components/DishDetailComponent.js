931fc4b3d5358d925f91fca3d0f2082dfcfc939c
Minh Hoang <daominhhoangggg@gmail.com>
2022-08-26 19:49:05 +0700

    Redux Actions & Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -18,6 +18,7 @@ import {
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { addComment } from '../redux/ActionCreators';
+import { Loading } from './LoadingComponent';
 
 function RenderDish({ dish }) {
   return (
@@ -162,7 +163,23 @@ class CommentForm extends Component {
 }
 
 const DishDetail = props => {
-  if (props.dish != null) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
